a33e7dba06554ec4a472d924eae5ff3efc230e76
David P. Steelman <dsteelma@umd.edu>
2025-09-26 08:01:04 -0400

    LIBDEVOPS-83. Added license comment to satisfy Maven "license:check"
    Added boiler-plate DRUM license notice to "postfix.sh" to satisfy
    the Maven "license:check" task that is run as part of GitHub action.
    
    Without this comment, the build fails with the following error:
    
    > Failed to execute goal com.mycila:license-maven-plugin:3.0:check
    > (check-headers) on project dspace: Some files do not have the expected
    > license header
    
    https://umd-dit.atlassian.net/browse/LIBDEVOPS-83

--- dspace/src/main/docker/cron/postfix.sh
+++ dspace/src/main/docker/cron/postfix.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#
+# The contents of this file are subject to the license and copyright
+# detailed in the LICENSE and NOTICE files at the root of the source
+# tree and available online at
+#
+# http://www.dspace.org/license/
+#
+
 # Prepare Postfix directories and start Postfix
 mkdir -p /var/spool/postfix/etc
 cp /etc/resolv.conf /var/spool/postfix/etc/
